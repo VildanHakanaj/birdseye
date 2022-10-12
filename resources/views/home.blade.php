04727e9a9d34a14c66f421f017d3d118010d85d1
Vildan Hakanaj <vildanhakanaj@gmail.com>
2022-10-11 21:47:26 -0400

    Added the small card styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,26 +12,271 @@
         <div class="trending">
             <h2 class="trending__heading">Trending</h2>
             <div class="trending__carousel">
-                <div class="trending__item">
-                    <img class="trending__image" src="{{url("images/thumbnails/beyond-earth/trending/large.jpg")}}"
+                <div class="movie-card movie-card--large">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/beyond-earth/trending/large.jpg")}}"
                          alt="Man on a rock in front of the water holding a camera.">
-                    <div class="trending__overlay">
-                        <div class="trending__bookmark">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
                             <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
                         </div>
-                        <div class="trending__play">
+                        <div class="movie-card__play">
                             <img src="{{url("images/icon-play.svg")}}" alt="">
                             <span>Play</span>
                         </div>
-                        <div class="trending__meta">
-                            <div class="trending__info">
+                        <div class="movie-card__meta">
+                            <div class="movie-card__info">
                                 2019 &dot; Movies &dot; PG
                             </div>
-                            <h2 class="trending__title">Beyond Earth</h2>
+                            <h2 class="movie-card__title">Beyond Earth</h2>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="movie-card movie-card--large">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/bottom-gear/trending/large.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                        <div class="movie-card__meta">
+                            <div class="movie-card__info">
+                                2021 &dot; Movies &dot; PG
+                            </div>
+                            <h2 class="movie-card__title">Bottom Gear</h2>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="movie-card movie-card--large">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/undiscovered-cities/trending/large.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                        <div class="movie-card__meta">
+                            <div class="movie-card__info">
+                                2019 &dot; Tv Series &dot; E
+                            </div>
+                            <h2 class="movie-card__title">Undiscovered Cities</h2>
                         </div>
                     </div>
 
                 </div>
             </div>
         </div>
+    <div class="recommended">
+        <h2 class="heading-lg text-white mb-32">Recommended for you</h2>
+        <div class="recommended__items">
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="movie-card movie-card--small">
+                    <img class="movie-card__image" src="{{url("images/thumbnails/the-great-lands/regular/small.jpg")}}"
+                         alt="Man on a rock in front of the water holding a camera.">
+                    <div class="movie-card__overlay">
+                        <div class="movie-card__bookmark">
+                            <img src="{{url("images/icon-bookmark-empty.svg")}}" alt="Bookmark icon">
+                        </div>
+                        <div class="movie-card__play">
+                            <img src="{{url("images/icon-play.svg")}}" alt="">
+                            <span>Play</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__meta">
+                    <div class="mb-5 card__info text-white body-sm">
+                        2019 &dot; Tv Series &dot; E
+                    </div>
+                    <h2 class="text-white heading-xs">Undiscovered Cities</h2>
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </div>
 </x-app-layout>
