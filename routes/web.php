930170c7bddd0b77eaccebbf0be1d3af1fb2670c
Vildan Hakanaj <vildanhakanaj@gmail.com>
2022-10-12 22:30:44 -0400

    Added the cateogry route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/{category}', [CategoryController::class, 'index']);
+
 
 require __DIR__.'/auth.php';
