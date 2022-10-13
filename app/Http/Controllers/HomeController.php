4406219eba307beefb1ff1339717cd42051037da
Vildan Hakanaj <vildanhakanaj@gmail.com>
2022-10-12 22:30:29 -0400

    Added the movie controller and model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request){
-
-        $movies = Movie::all();
-        return view("home", compact('movies'));
+    public function index()
+    {
+        return view("home", [
+            "movies" => Movie::all()
+        ]);
 
     }
 }
